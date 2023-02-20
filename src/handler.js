b946a5b3ba34600c5f58c97e58986e0fb4965f2b
tiny-dust <idioticzhou@foxmail.com>
2023-02-20 10:52:14 +0800

    feat(handler): 编写处理脚本

--- src/handler.js
+++ src/handler.js
@@ -1,38 +1,48 @@
 // 根据询问的结果 进行相应的操作
 const { exec } = require('child_process');
+const shellColor = require('./utils/color');
+
+const createTag = async (version) => {
+  const tag = `v${version}`;
+  // 修改package中的version
+  exec(`git tag ${tag}`, (error) => {
+    if (error) {
+      console.log(shellColor.red(`🚫 创建tag失败: ${error}`));
+      process.exit(1);
+    }
+    console.log(shellColor.green(`🎉 创建tag成功: ${tag}`));
+    // 修改package.json文件中的version
+    exec(`npm version ${version}`, (err) => {
+      if (err) {
+        console.log(shellColor.red(`🚫 修改package.json中的version失败: ${err}`));
+      }
+    });
+  });
+};
+
+const handlerEvent = new Map([
+  ['publish', async () => {
+    console.log('发布');
+  }],
+  ['release', async () => {
+    console.log('发布');
+  }],
+  ['push', async () => {
+    console.log('发布');
+  }],
+  ['changelog', async (version) => {
+    console.log('version', version);
+  }],
+  ['tag', createTag],
+]);
 
 const resultHandler = async (result) => {
-  console.log('5-「handler」', result);
-  const {
-    publish, version: { type, version: newVersion }, release, push, changelog, tag,
-  } = result;
-  // 发布
-  if (publish) {
-    await exec('npm publish');
-  }
-  // 升级版本号
-  if (type) {
-    await exec(`npm version ${type}`);
-  }
-  // 创建 changelog
-  if (changelog) {
-    await exec('conventional-changelog -p angular -i CHANGELOG.md -s');
-  }
-  // 推送到远程仓库
-  if (push) {
-    await exec('git add .');
-    await exec(`git commit -m "chore: ${newVersion}"`);
-    await exec('git push');
-  }
-  // 创建tag
-  if (tag) {
-    await exec(`git tag v${newVersion}`);
-    await exec('git push --tags');
-  }
-  // 发行
-  if (release) {
-    await exec('npm run release');
-  }
+  const { version: { version: newVersion }, ...other } = result;
+
+  // 执行操作
+  Object.keys(other).forEach(async (key) => {
+    if (other[key]) await handlerEvent.get(key)(newVersion);
+  });
 };
 
 module.exports = resultHandler;
